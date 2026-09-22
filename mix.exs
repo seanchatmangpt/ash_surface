0966309f6a56c7bac0b62f237ae31f18f606f607
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-09-21 19:07:58 -0700

    chore(release): advance ash_surface to 26.9.21

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule AshSurface.MixProject do
   use Mix.Project
 
-  @version "26.9.13"
+  @version "26.9.21"
   @source_url "https://github.com/seanchatmangpt/ash_surface"
 
   def project do
